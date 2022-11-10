2644dc2015f3c3436ad763b24fec11c8b485cd01
Danielateresa <dany.t_87@hotmail.it>
2022-11-10 18:36:46 +0100

    Completed footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -47,78 +47,94 @@ export default {
         </div>
         <!-- /.blue_bar-->
 
-        <div class="upper_footer pt-4 pb-5">
+        <div class="upper_footer">
             <div class="container">
-                <div class="row">
-                    <div class="col-4 left_col">
-                        <div class="row ">
-                            <div class="col-4">
-                                <h5>
-                                    dc comics
-                                </h5>
-                                <ul>
-                                    <li><a href="">Characters</a></li>
-                                    <li><a href="">Comics</a></li>
-                                    <li><a href="">Movies</a></li>
-                                    <li><a href="">TV</a></li>
-                                    <li><a href="">Games</a></li>
-                                    <li><a href="">Videos</a></li>
-                                    <li><a href="">News</a></li>
-                                </ul>
-                                <h5>
-                                    shop
-                                </h5>
-                                <ul>
-                                    <li><a href="">Shop DC</a></li>
-                                    <li><a href="">Shop DC Collectibles</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-4">
-                                <h5>
-                                    dc
-                                </h5>
-                                <ul>
-                                    <li><a href="">Terms Of Use</a></li>
-                                    <li><a href="">Privacy Policy (new)</a></li>
-                                    <li><a href="">Ad Choices</a></li>
-                                    <li><a href="">Advertising</a></li>
-                                    <li><a href="">Jobs</a></li>
-                                    <li><a href="">Subscription</a></li>
-                                    <li><a href="">Talent Workshops</a></li>
-                                    <li><a href="">CPSC Certificates</a></li>
-                                    <li><a href="">Ratings</a></li>
-                                    <li><a href="">Shop Help</a></li>
-                                    <li><a href="">Contact Us</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-4">
-                                <h5>
-                                    sites
-                                </h5>
-                                <ul>
-                                    <li><a href="">DC</a></li>
-                                    <li><a href="">MAD Magazine</a></li>
-                                    <li><a href="">DC Kids</a></li>
-                                    <li><a href="">DC Universe</a></li>
-                                    <li><a href="">DC Power Visa</a></li>
+                <div class="logo_layer_footer pt-4 pb-5">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4 left_col">
+                                <div class="row ">
+                                    <div class="col-4">
+                                        <h5>
+                                            dc comics
+                                        </h5>
+                                        <ul>
+                                            <li><a href="">Characters</a></li>
+                                            <li><a href="">Comics</a></li>
+                                            <li><a href="">Movies</a></li>
+                                            <li><a href="">TV</a></li>
+                                            <li><a href="">Games</a></li>
+                                            <li><a href="">Videos</a></li>
+                                            <li><a href="">News</a></li>
+                                        </ul>
+                                        <h5>
+                                            shop
+                                        </h5>
+                                        <ul>
+                                            <li><a href="">Shop DC</a></li>
+                                            <li><a href="">Shop DC Collectibles</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-4">
+                                        <h5>
+                                            dc
+                                        </h5>
+                                        <ul>
+                                            <li><a href="">Terms Of Use</a></li>
+                                            <li><a href="">Privacy Policy (new)</a></li>
+                                            <li><a href="">Ad Choices</a></li>
+                                            <li><a href="">Advertising</a></li>
+                                            <li><a href="">Jobs</a></li>
+                                            <li><a href="">Subscription</a></li>
+                                            <li><a href="">Talent Workshops</a></li>
+                                            <li><a href="">CPSC Certificates</a></li>
+                                            <li><a href="">Ratings</a></li>
+                                            <li><a href="">Shop Help</a></li>
+                                            <li><a href="">Contact Us</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-4">
+                                        <h5>
+                                            sites
+                                        </h5>
+                                        <ul>
+                                            <li><a href="">DC</a></li>
+                                            <li><a href="">MAD Magazine</a></li>
+                                            <li><a href="">DC Kids</a></li>
+                                            <li><a href="">DC Universe</a></li>
+                                            <li><a href="">DC Power Visa</a></li>
 
-                                </ul>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
         <!-- /.upper_footer-->
 
         <div class="bottom_footer">
-            <div class="container">
+            <div class="container d-flex justify-content-between">
                 <div class="bottom_left">
-
+                    <button class="btn">
+                        sign-up now!
+                    </button>
                 </div>
                 <div class="bottom_right">
-
+                    <div class="social d-flex align-items-center">
+                        <h5 class="me-4">follow us</h5>
+                        <img src="../assets/img/footer-facebook.png" alt="facebook logo">
+                        <img src="../assets/img/footer-twitter.png" alt="twitter logo">
+                        <img src="../assets/img/footer-youtube.png" alt="youtube logo">
+                        <img src="../assets/img/footer-pinterest.png" alt="pinterest logo">
+                        <img src="../assets/img/footer-periscope.png" alt="position logo">
+                    </div>
                 </div>
+
+
             </div>
         </div>
         <!-- /.bottom_footer-->
