16700794bd8796888b20e32941e71ac1e3a1a654
Danielateresa <dany.t_87@hotmail.it>
2022-11-11 15:18:39 +0100

    Added cards in main

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -10,12 +10,113 @@ export default {
 
 <template>
     <main id="site_main">
-        <div class="container">
-            <h1>
-                -->Content goes here-- </h1>
+        <div class="comics_jumbotron">
+
         </div>
+        <!-- .comics_jumbotron -->
+
+        <div class="container cards px-4 pb-4">
+            <div class="tag">
+                <span>current series</span>
+            </div>
+            <div class="row px-4 d-flex">
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col-2 mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+                <div class="col mt-5">
+                    <div class="comic_card">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                        <span>action comics</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="button_box text-center mt-5">
+                <button class="btn load_more">
+                    load more
+                </button>
+            </div>
+        </div>
+        <!-- ./cards -->
 
         <BlueSection />
+
+
 
     </main>
     <!-- /#site_main -->
